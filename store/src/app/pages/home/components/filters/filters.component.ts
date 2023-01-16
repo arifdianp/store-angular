9ba22f45cf6dde1c68f3884f92bb8885b31f3364
Arif Dianprakasa <dparief32@gmail.com>
2023-01-16 15:47:36 +0700

    testing with views of product image and desc

--- store/src/app/pages/home/components/filters/filters.component.ts
+++ store/src/app/pages/home/components/filters/filters.component.ts
@@ -2,14 +2,17 @@ import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/cor
 
 @Component({
   selector: 'app-filters',
-  templateUrl: './filters.component.html'
+  templateUrl: 'filters.component.html'
 })
-export class FiltersComponent
+export class FiltersComponent implements OnInit, OnDestroy
 {
   categories = ['shoes','sports'];
   @Output() showCategory = new EventEmitter<string>();
 
+  ngOnInit(): void {}
+  ngOnDestroy(): void {}
+
   onShowCategory(category: string): void {
-    this.showCategory.next(category);
+    this.showCategory.emit(category);
   }
 }
--- store/src/app/pages/home/home.component.ts
+++ store/src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
@@ -9,6 +11,7 @@ export class HomeComponent implements OnInit, OnDestroy
 {
   cols = 3;
   category: string | undefined;
+  rowHeight: number = ROWS_HEIGHT[this.cols];
 
   ngOnInit(): void {}
   ngOnDestroy(): void {}
